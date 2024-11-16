510e8ddbb2ce2dcd99df1685fd73ebed053dbaea
Nandagopalvs25 <nandagopalvs12@gmail.com>
2024-11-16 21:11:50 +0530

    Cors fix

--- backend/settings.py
+++ backend/settings.py
@@ -178,7 +178,8 @@ SITE_ID = 1
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+     'https://mismatch-frontend.vercel.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
